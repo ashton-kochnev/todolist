18ecb92b3818b77e4cac13a763c8b273c6a7558f
ashton kochnev <eminem19901111@gmail.com>
2021-05-24 17:16:03 +0300

    Updates

---
+++ precache-manifest.0cdbcd6b9150a6915bd8e3a43bfaf2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d0cae9c5c4fb14492a347c924850d5",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "e2937162d13e1fbda2f3",
+    "url": "/todolist/static/css/main.01955cc3.chunk.css"
+  },
+  {
+    "revision": "37056366b208ca4f8e57",
+    "url": "/todolist/static/js/2.4f8d20ec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todolist/static/js/2.4f8d20ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2937162d13e1fbda2f3",
+    "url": "/todolist/static/js/main.2f1d4813.chunk.js"
+  },
+  {
+    "revision": "471e54e937a3aef18e16",
+    "url": "/todolist/static/js/runtime-main.283d8678.js"
+  }
+]);
